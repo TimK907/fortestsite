981b21b3a505c5569101b89450ca0dcacab3a362
TimK907 <artem_karpenko3@epam.com>
2025-06-23 05:12:48 +0300

    Update msal.js

--- public/msal.js
+++ public/msal.js
@@ -1,1 +1,2 @@
-
+/*! @azure/msal-browser v2.37.2 2024-01-30 */
+!function(e,r){"object"==typeof exports&&"object"==typeof module?module.exports=r():"function"==typeof define&&define.amd?define([],r):"object"==typeof exports?exports.msal=r():e.msal=r()}(this,function(){/*...*/});
