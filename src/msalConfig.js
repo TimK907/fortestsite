9d960cce0eff58e45e85a8b8e7f4c930fc095b3a
TimK907 <artem_karpenko3@epam.com>
2025-06-23 08:27:01 +0300

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
-        clientId: "05bdb70e-0233-4120-8a95-5e64e9801c84",
+        clientId: "05bdb70e-0233-4120-8a95-5e64e9801c84", // App#1
         authority: "https://login.microsoftonline.com/d06629ae-56db-44af-880b-80afafa24182",
-        redirectUri: window.location.origin
+        redirectUri: window.location.origin,
     },
     cache: {
         cacheLocation: "sessionStorage",
@@ -10,11 +10,12 @@ export const msalConfig = {
     },
 };
 
+// Додаємо scope саме до ресурсу API бота (App#2)
 export const loginRequest = {
     scopes: [
         "openid",
         "profile",
         "email",
-        "api://bcbeea24-4189-43f8-9360-65529eb7a556/user_impersonation"  // <= це токен для бота
+        "api://bcbeea24-4189-43f8-9360-65529eb7a556/user_impersonation"
     ],
 };
